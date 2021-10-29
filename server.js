02900a0db04e1a6919373048b85291b6315443d4
Luis <iluisflores@yahoo.com>
2021-10-28 22:38:56 -0500

    "Connected to MongoDB"

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 require('dotenv').config();
 const { PORT = 3001, DATABASE_URL } = process.env;
+
+mongoose.connect(DATABASE_URL);
+
+const db = mongoose.connection;
+
+db.on('connected', () => console.log('Connected to MongoDB'));
+db.on('disconnected', () => console.log('Disconnected to MongoDB'));
+db.on('error', () => console.log('MongoDB has an error ' + error.message));
+
+
+
 
 app.listen(PORT, () => console.log(`Express is listening on Port: ${PORT}`));
